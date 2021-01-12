236ce4362402166fb8829c940819189cb6ae0439
samergain <samersadon@gmail.com>
2021-01-11 22:14:36 -0500

    update

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 //set the current DateTime in header
 var currentDT = moment().format('MMMM Do YYYY, h:mm:ss a');
 $("#currentDay").text(currentDT);
+styleTxtArea();
+getLocStorage();
 
-//setting classes for textAreas based on the current time
-//grab all textAreas in an array, get the value for each, prepare jQuery selector string,
-//compare each val with currentHour and set class accordingly
-var currentTime = moment().format();
-var currentHour = currentTime.split("T")[1].split(":")[0];
-var hourBoxs = $("textarea");
-for (var i=0; i<hourBoxs.length; i++) {
-    var txtVal = hourBoxs[i].getAttribute("value");
-    var areaWithVal = `textarea[value ='` + txtVal + `']`;
-    if (parseInt(txtVal) < parseInt(currentHour)){
-        $(areaWithVal).addClass("past");
-    } else if (parseInt(txtVal) > parseInt(currentHour)){
-        $(areaWithVal).addClass("future");
-    } else {
-        $(areaWithVal).addClass("present");
-    }
-}
 
 //key and val to save data to localStorage
 var locStorageKey = "";
@@ -32,15 +17,44 @@ $(".btn").on("click",function(){
     localStorage.setItem(locStorageKey,locStorageVal);
 });
 
-
+//clear data button e.handler
+// $("#clear").on("click", resetData);
+//setting classes for textAreas based on the current time
+//grab all textAreas in an array, get the value for each, prepare jQuery selector string,
+//compare each val with currentHour and set class accordingly
+function styleTxtArea(){
+    var currentTime = moment().format();
+    var currentHour = currentTime.split("T")[1].split(":")[0];
+    var hourBoxs = $("textarea");
+    for (var i=0; i<hourBoxs.length; i++) {
+        var txtVal = hourBoxs[i].getAttribute("value");
+        var areaWithVal = `textarea[value ='` + txtVal + `']`;
+        if (parseInt(txtVal) < parseInt(currentHour)){
+            $(areaWithVal).addClass("past");
+        } else if (parseInt(txtVal) > parseInt(currentHour)){
+            $(areaWithVal).addClass("future");
+        } else {
+            $(areaWithVal).addClass("present");
+        }
+    }
+}
 //loop on localStorage to get the available data on page
-for (var i=0; i<localStorage.length; i++) {
-    var keyStorage = localStorage.key(i);
-    var valueStorage = localStorage.getItem(keyStorage);
-    //assign localStorage to txtAreas 
-    var areaSelector = `textarea[value='` + keyStorage + `']`;
-    $(areaSelector).val(valueStorage);   
+function getLocStorage(){
+    for (var i=0; i<localStorage.length; i++) {
+        var keyStorage = localStorage.key(i);
+        var valueStorage = localStorage.getItem(keyStorage);
+        //assign localStorage to txtAreas 
+        var areaSelector = `textarea[value='` + keyStorage + `']`;
+        $(areaSelector).val(valueStorage);   
+    }
 }
 
-
+//clear all entered data
+// function resetData() {
+//     var hourBoxs = $("textarea");
+//     for (var i=0; i<hourBoxs.length; i++) {
+//         document.getElementsByTagName("textarea")[i].setAttribute("value", ""); 
+// }
+// localStorage.clear();
+// }
 
